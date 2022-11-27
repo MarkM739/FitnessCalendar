dbbec757f1d2c77392ed5a5fc1e828c1f1599447
Mark Murphy <108417914+MarkM739@users.noreply.github.com>
2022-11-27 15:48:06 -0330

    Commit test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+
+const element =<h1>Fitness Calendar</h1>;
+ReactDOM.render(element, document.getElementById("root"));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div>
+  Hi
+ </div>
   );
 }
 
---
+++ src/components/calendar.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+class Calendar extends React.Component {
+    // @ts-ignore
+    constructor(props) { 
+        super (props)
+        this.state = {Date}
+    }
+    render() { 
+        this.setState 
+        return (  );
+    }
+}
+ 
+export default ;
+
